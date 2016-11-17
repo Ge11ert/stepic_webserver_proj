e89d00ab7c4caef1bcde00446257a392d30cfe4a
Ge11ert <gellert94@mail.ru>
2016-11-17 22:36:26 +0300

    ver 2.1

--- init.sh
+++ init.sh
@@ -1,5 +1,2 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
